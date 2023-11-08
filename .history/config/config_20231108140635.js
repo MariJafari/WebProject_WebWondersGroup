b408f6b45a3c4ba61407ea052ce5ee44b362cf10
Mari Jafari <125914019+MariJafari@users.noreply.github.com>
2023-11-08 14:10:19 -0500

    edit DB

--- .history/config/config_20231108140635.js
+++ .history/config/config_20231108140635.js
@@ -11,13 +11,13 @@ process.env.MONGO_HOST ||
 export default config*/
 
 const config = {
-    env: process.env.NODE_ENV || 'development', 
-    port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
-    mongoUri: process.env.MONGODB_URI || "mongodb+srv://mjafari7:NelsZbcBLSRiXBcZ@bookstore.b3sblxc.mongodb.net/"||
+ env: process.env.NODE_ENV || 'development', 
+ port: process.env.PORT || 3000,
+ jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", 
+ mongoUri: process.env.MONGODB_URI || "mongodb+srv://mjafari7:NelsZbcBLSRiXBcZ@bookstore.b3sblxc.mongodb.net/"||
 process.env.MONGO_HOST ||
-'mongodb://' + (process.env.IP || 'localhost') + ':' + 
+ 'mongodb://' + (process.env.IP || 'localhost') + ':' + 
 (process.env.MONGO_PORT || '27017') +
-'/mernproject' 
-}
-export default config
+ '/mernproject' 
+ }
+ export default config
